c4fd8fad837d4c37f4bc81d42252f928da088915
Adedapo Aderemi <adedapopaul@yahoo.com>
2023-12-11 22:14:54 +0000

    add github action and github issue template

--- sh/dockerpush.sh
+++ sh/dockerpush.sh
@@ -3,12 +3,12 @@
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/../" &> /dev/null
 
 IMAGE_TAG="${IMAGE_TAG:-$(make -s name)}"
-IMAGE_NAME="pos-dashboard:${IMAGE_TAG}"
-DOCKER_LOGIN_SERVER="acclabdocker.azurecr.io"
+IMAGE_NAME="pos-dashboard:v.1.0"
+DOCKER_LOGIN_SERVER="thispama"
 DOCKER_IMAGE_URL="${DOCKER_LOGIN_SERVER}/${IMAGE_NAME}"
 
 echo "pushing ${IMAGE_NAME} to ${DOCKER_IMAGE_URL}"
 
 docker tag "${IMAGE_NAME}" "${DOCKER_IMAGE_URL}"
 docker push "${DOCKER_IMAGE_URL}"
-docker rmi "${DOCKER_IMAGE_URL}"
+# docker rmi "${DOCKER_IMAGE_URL}"
