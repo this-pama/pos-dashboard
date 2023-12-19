5d574600496c71eb26220e99609fd065ac41d1ea
Adedapo Aderemi <adedapopaul@yahoo.com>
2023-12-19 16:36:01 +0000

    update subnet ids

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,13 +24,32 @@ module "vpc" {
 module "eks_dev" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "eks-dev-cluster"
-  subnets         = module.vpc.private_subnets
+  subnet_ids         = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
   cluster_version = "1.21"
 
   cluster_endpoint_public_access = true
 }
 
+module "eks_staging" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "eks-staging-cluster"
+  subnet_ids         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+  cluster_version = "1.21"
+
+  cluster_endpoint_public_access = true
+}
+
+module "eks_prod" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "eks-prod-cluster"
+  subnet_ids         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+  cluster_version = "1.21"
+
+  cluster_endpoint_public_access = true
+}
 
 output "vpc_id" {
   value = module.vpc.vpc_id
@@ -44,4 +63,10 @@ output "eks_dev_cluster_id" {
   value = module.eks_dev.cluster_id
 }
 
+output "eks_staging_cluster_id" {
+  value = module.eks_staging.cluster_id
+}
 
+output "eks_prod_cluster_id" {
+  value = module.eks_prod.cluster_id
+}
