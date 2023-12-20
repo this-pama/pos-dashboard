b757dad9d59cdd0e1c0605aed727a617a4917076
Adedapo Aderemi <adedapopaul@yahoo.com>
2023-12-20 22:20:55 +0000

    update

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -594,6 +594,19 @@ resource "kubernetes_service" "postgres_service" {
 # PROMETHEUS RESOURCE AND SERVICE
 ################################################################################
 
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
+}
+
 resource "helm_release" "prometheus" {
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
