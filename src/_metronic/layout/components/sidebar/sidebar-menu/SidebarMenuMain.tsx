9fa93ca48807cdf9e2ebb24d3de30ef572be5854
Adedapo Aderemi <adedapopaul@yahoo.com>
2024-01-06 18:51:23 +0000

    Update sidebar

--- src/_metronic/layout/components/sidebar/sidebar-menu/SidebarMenuMain.tsx
+++ src/_metronic/layout/components/sidebar/sidebar-menu/SidebarMenuMain.tsx
@@ -13,12 +13,8 @@ const SidebarMenuMain = () => {
         title={intl.formatMessage({id: 'MENU.DASHBOARD'})}
         fontIcon='bi-app-indicator'
       />
-      <SidebarMenuItem to='/dashboard' icon='switch' title='Orders' fontIcon='bi-layers' />
-      <div className='menu-item'>
-        <div className='menu-content pt-8 pb-2'>
-          <span className='menu-section text-muted text-uppercase fs-8 ls-1'>Crafted</span>
-        </div>
-      </div>
+      
+      
       
       <div className='menu-item'>
         <div className='menu-content pt-8 pb-2'>
@@ -27,7 +23,7 @@ const SidebarMenuMain = () => {
       </div>
       <SidebarMenuItem
         to='/'
-        title='Chat'
+        title='Orders'
         fontIcon='bi-chat-left'
         icon='message-text-2'
       />
@@ -37,18 +33,7 @@ const SidebarMenuMain = () => {
         title='User management'
         fontIcon='bi-layers'
       />
-      <div className='menu-item'>
-        <a
-          target='_blank'
-          className='menu-link'
-          href='#'
-        >
-          <span className='menu-icon'>
-            <KTIcon iconName='code' className='fs-2' />
-          </span>
-          <span className='menu-title'>Logout</span>
-        </a>
-      </div>
+      
     </>
   )
 }
